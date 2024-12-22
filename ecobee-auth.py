012aa71acbf3ce16e6862c5d6e8726a506b010cb
Matthew Cross <matt.cross@gmail.com>
2024-12-22 16:19:37 -0500

    Add ecobee support library.

--- ecobee-auth.py
+++ ecobee-auth.py
@@ -1,79 +1,31 @@
 #!/usr/bin/env python3
 
 from collections import namedtuple
+import jhf.ecobee
 import jhf.utils
 import sys
-import time
-import urllib.error
 
-AUTH_TOKEN_FILE="ecobee_token.json"
-
-def auth_token(config):
-    """return current access token after refreshing, or None on failure"""
-
-    try:
-        token_json = open(AUTH_TOKEN_FILE, "r").read()
-
-        token = jhf.utils.parse_json(token_json)
-
-        refreshed_token_json = jhf.utils.fetch_url(
-            f"https://api.ecobee.com/token?grant_type=refresh_token&refresh_token={token.refresh_token}&client_id={config.apikey}&ecobee_type=jwt",
-            "ecobee refresh endpoint")
-
-        refreshed_token = jhf.utils.parse_json(refreshed_token_json)
-
-        if len(refreshed_token.access_token) > 0:
-            # All good - token was refreshed, and refresh succeeded
-            open(AUTH_TOKEN_FILE, "w").write(refreshed_token_json.decode("utf-8"))
-            return refreshed_token.access_token
-
-    except:
-        # Something failed during the refresh, so just return None
-        return None
-    
 def main():
     config = jhf.utils.Config().ecobee
 
-    if auth_token(config):
-        print("Already authenticated!")
-        return 0
-
-    auth = jhf.utils.fetch_url_as_json(
-        f"https://api.ecobee.com/authorize?response_type=ecobeePin&client_id={config.apikey}&scope=smartWrite",
-        desc = "ecobee authentication endpoint")
-
-    print(f"reply {auth}")
-
-    print(f"""To authenticate this app with Ecobee, log into your Ecobee
+    def display_pin(pin):
+        print(
+            f"""To authenticate this app with Ecobee, log into your Ecobee
 account at www.ecobee.com, in the pull-down menu from the upper right
 click on 'My Apps', click 'Add Application', and enter this PIN code:
 
-{auth.ecobeePin}""")
+{pin}""")
 
-
-    while True:
-        time.sleep(auth.interval)
-
-        try:
-            token_string = jhf.utils.fetch_url(
-                f"https://api.ecobee.com/token?grant_type=ecobeePin&code={auth.code}&client_id={config.apikey}&ecobee_type=jwt",
-                desc = "ecobee token endpoint")
-        except urllib.error.HTTPError as err:
-            token_string = ""
-
-        try:
-            token = jhf.utils.parse_json(token_string)
-            if len(token.refresh_token) != 0:
-                f = open(AUTH_TOKEN_FILE, "w")
-                f.write(token_string.decode("utf-8"))
-                f.close()
-
-                print(f"Authenticated!  Token written to {AUTH_TOKEN_FILE}")
-                return 0
-        except AttributeError:
-            pass
-
+    def working():
         print(".", end="", flush=True)
+        return True
+
+    if jhf.ecobee.authenticate(config, display_pin, working):
+        print("Authenticated successfully!")
+        return 0
+    else:
+        print("Failed authentication")
+        return 1
 
 if __name__ == "__main__":
     sys.exit(main())
---
+++ jhf/ecobee.py
@@ -0,0 +1,79 @@
+from . import utils
+import time
+import urllib.error
+
+auth_token_file="ecobee_token.json"
+
+def auth_token(config):
+    """return current access token after refreshing, or None on failure"""
+
+    try:
+        token_json = open(auth_token_file, "r").read()
+
+        token = utils.parse_json(token_json)
+
+        refreshed_token_json = utils.fetch_url(
+            f"https://api.ecobee.com/token?grant_type=refresh_token&refresh_token={token.refresh_token}&client_id={config.apikey}&ecobee_type=jwt",
+            "ecobee refresh endpoint")
+
+        refreshed_token = utils.parse_json(refreshed_token_json)
+
+        if len(refreshed_token.access_token) > 0:
+            # All good - token was refreshed, and refresh succeeded
+            open(auth_token_file, "w").write(refreshed_token_json.decode("utf-8"))
+            return refreshed_token.access_token
+
+    except:
+        # Something failed during the refresh, so just return None
+        return None
+    
+def authenticate(config, pin_display_callback, working_callback):
+    """Authenticate with ecobee using API key in config.
+
+    The overall protocol requires the user to log in to ecobee, go the
+    'My Apps' menu, add an app (which requires providing the
+    dynamically generated PIN code), and approve it.
+
+    To accomplish this, this function will call the
+    pin_display_callback with the PIN when it receives it so that it
+    can be displayed to the user.  While working it will will call
+    working_callback periodically.
+
+    If the working callback returns a non-truthy value, the
+    authentication will be aborted and this function will return.
+
+    Returns True on authentication success, and False on failure."""
+
+    if auth_token(config):
+        # Already authenticated
+        return True
+
+    auth = utils.fetch_url_as_json(
+        f"https://api.ecobee.com/authorize?response_type=ecobeePin&client_id={config.apikey}&scope=smartWrite",
+        desc = "ecobee authentication endpoint")
+
+    pin_display_callback(auth.ecobeePin)
+
+    while True:
+        time.sleep(auth.interval)
+
+        try:
+            token_string = utils.fetch_url(
+                f"https://api.ecobee.com/token?grant_type=ecobeePin&code={auth.code}&client_id={config.apikey}&ecobee_type=jwt",
+                desc = "ecobee token endpoint")
+        except urllib.error.HTTPError as err:
+            token_string = ""
+
+        try:
+            token = utils.parse_json(token_string)
+            if len(token.refresh_token) != 0:
+                f = open(auth_token_file, "w")
+                f.write(token_string.decode("utf-8"))
+                f.close()
+
+                return True
+        except AttributeError:
+            pass
+
+        if not working_callback():
+            return False
